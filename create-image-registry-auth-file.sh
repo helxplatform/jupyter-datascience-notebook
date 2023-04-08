3ace07dc7fd8a03f6de03f64183ab3d0decc5234
PJ Linebaugh <pjl@renci.org>
2023-04-08 01:22:14 -0400

    add ability to use external config file for image registry

--- create-image-registry-auth-file.sh
+++ create-image-registry-auth-file.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Create a .image-registry-auth-config-env.sh file with environment variables
-# set for REGISTRY_USERNAME and REGISTRY_PASSWORD.  Using a password manager
-# like LastPass with it's lpass CLI program to set the variables would be good
-# for this.
+# set for REGISTRY_USERNAME, REGISTRY_PASSWORD and IMAGE_REGISTRY (optional).
+# Using a password manager like LastPass with it's lpass CLI program to set the
+# variables would be good for this.
 source .image-registry-auth-config-env.sh
 
-# IMAGE_REGISTRY="https://index.docker.io/v1/"
-IMAGE_REGISTRY="https://containers.renci.org/v1/"
+IMAGE_REGISTRY=${IMAGE_REGISTRY-"https://index.docker.io/v1/"}
 CONFIG=.image-registry-auth-config.json
 touch $CONFIG
 chmod 600 $CONFIG
